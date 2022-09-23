3201ebc65ba6642ee9203970dd6c78e3621f6f27
Md Al Islam <61063541+PIslam19@users.noreply.github.com>
2022-09-23 14:43:50 +0600

    Add files via upload

---
+++ CVController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurriculumVitae.Controllers
+{
+    public class CVController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Career()
+        {
+            return View();
+        }
+        public ActionResult Academic()
+        {
+            return View();
+        }
+        public ActionResult Skill()
+        {
+            return View();
+        }
+    }
+}
